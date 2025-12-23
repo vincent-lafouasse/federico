f7a8259b9fbec4d82e54210b967f417383ea676c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-23 21:47:46 +0100

    sketch register accessor macros

--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 
 #define FIRMWARE_PATH "./firmware/busicom.bin"
 
+#define GET_REG4(regs, i) (((regs) >> ((i) << 2)) & 0xF)
+#define SET_REG4(regs, i, val)                     \
+    ((regs) = ((regs) & ~(0xFULL << ((i) << 2))) | \
+              (((uint64_t)(val) & 0xF) << ((i) << 2)))
+
+#define GET_REG8(regs, i) (((regs) >> ((i) << 3)) & 0xFF)
+
+#define SET_REG8(regs, i, val)                      \
+    ((regs) = ((regs) & ~(0xFFULL << ((i) << 3))) | \
+              (((uint64_t)(val) & 0xFF) << ((i) << 3)))
+
 typedef struct {
     uint64_t registers;
     uint16_t pc;
